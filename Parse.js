11f3131af7c89c49a96a5a2dc87542de19c14d10
andrea mangrella <mangrella.andrea@gmail.com>
2022-06-17 11:09:36 +0200

    added firefox user agent

--- Parse.js
+++ Parse.js
@@ -1,39 +1,42 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
 
-export default async function Parse(topic){
+const user_agents = [];
+user_agents[0] = // fatto da firefox, n
+  "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:101.0) Gecko/20100101 Firefox/101.0";
+user_agents[1] = // fatto da chrome
+  "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.9999.0 Safari/537.36";
+// secondo me chrome contiene safari, firefox e chrome, da ritestare
+
+export default async function Parse(topic) {
   let res = [];
-  const val = await axios.get(
-    `https://scholar.google.com/scholar?hl=it&as_sdt=0%2C5&q=${topic}&btnG=`,
-    {
-      Headers: {
-        "user-agent":
-          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.9999.0 Safari/537.36",
-      },
-    }
-  );
-  const $ = cheerio.load(val.data);
-  const $$ = cheerio.load(val.data);
-  let x,y;
-  let appo,temp1,temp2;
-  let i=0;
-  do {
-    x = $("div.gs_or_ggsm:first");
-    y = $$("div.gs_ri:first");
-    $("div.gs_ggsd:first").remove();
-    $$("div.gs_ri:first").remove();
-    if (x.html() != null && y.html() != null) {
-      if (
-        !x.text().includes("Full View") ||
-        !x.text().includes("ACNP Full Text")
-      ) {
-        appo = x.find("a").attr("href");
-        temp1 = y.find("h3.gs_rt").text();
-        temp2 = y.find("div.gs_a").text();
-        res[i] = {"url": appo,"title":temp1,"authors":temp2 };
+  try {
+    const val = await axios.get(
+      `https://scholar.google.com/scholar?hl=it&as_sdt=0%2C5&q=${topic}&btnG=`,
+      {
+        Headers: {
+          "user-agent": user_agents[Math.random() * 1],
+        },
       }
-    }
-    i++;
-  } while (x.html() != null);
+    );
+    const $ = cheerio.load(val.data);
+    const $$ = cheerio.load(val.data);
+    let x, y;
+    let i = 0;
+    do {
+      x = $("div.gs_or_ggsm:first");
+      y = $$("div.gs_ri:first");
+      if (x.html() != null && y.html() != null) {
+        res[i] = {
+          url: x.find("a").attr("href"),
+          title: y.find("h3.gs_rt").text(),
+          authors: y.find("div.gs_a").text(),
+        };
+      }
+      $("div.gs_ggsd:first").remove();
+      $$("div.gs_ri:first").remove();
+      i++;
+    } while (x.html() != null);
+  } catch (err) {}
   return res;
-};
+}
